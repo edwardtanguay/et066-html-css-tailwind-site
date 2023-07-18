537f0c99b31cfa21af016e0c2686c06183cd29a2
Edward Tanguay (windows) <edward@tanguay.info>
2023-07-18 10:17:00 +0200

    basics for overriding

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,20 @@ export const content = [
   "./**/*.html"
 ];
 export const theme = {
-  extend: {},
+  colors: {
+    'blue': '#333',
+    'purple': '#7e5bef',
+    'pink': '#ff49db',
+    'orange': '#ff7849',
+    'green': '#13ce66',
+    'yellow': '#ffc82c',
+    'gray-dark': '#273444',
+    'gray': '#8492a6',
+    'gray-light': '#d3dce6',
+    'ok': '#222'
+  },
+  extend: {
+  },
 };
 export const plugins = [];
 
