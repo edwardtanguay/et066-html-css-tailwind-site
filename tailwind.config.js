ba1145737593f471b48a63d258adeb9e65e0c190
Edward Tanguay (windows) <edward@tanguay.info>
2023-07-18 10:22:46 +0200

    custom style in

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,6 @@ export const content = [
   "./**/*.html"
 ];
 export const theme = {
-  colors: {
-    'blue': '#333',
-    'purple': '#7e5bef',
-    'pink': '#ff49db',
-    'orange': '#ff7849',
-    'green': '#13ce66',
-    'yellow': '#ffc82c',
-    'gray-dark': '#273444',
-    'gray': '#8492a6',
-    'gray-light': '#d3dce6',
-    'ok': '#222'
-  },
   extend: {
   },
 };
